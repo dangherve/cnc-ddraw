31ddfa3dc58b74afc35409b3239ecd3daad0b4a7
dangherve <dangherve@gmail.com>
2026-03-07 01:17:59 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/FunkyFr3sh/cnc-ddraw.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/cnc-ddraw.git
+git push --force --set-upstream origin master
